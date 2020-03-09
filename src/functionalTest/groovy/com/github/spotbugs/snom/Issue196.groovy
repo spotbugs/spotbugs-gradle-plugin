188563b6cfac3a1db1bec46781c86b5f3fcebc76
Kengo TODA <skypencil@gmail.com>
2020-03-09 12:30:38 +0800

    test: try to reported case, but failed
    https://github.com/spotbugs/spotbugs-gradle-plugin/issues/196\#issuecomment-596326144

---
+++ src/functionalTest/groovy/com/github/spotbugs/snom/Issue196.groovy
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2019 SpotBugs team
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.spotbugs.snom
+
+import org.gradle.internal.impldep.com.google.common.io.Files
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.gradle.util.GradleVersion
+import org.junit.jupiter.api.BeforeEach
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.nio.file.Paths
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import static org.junit.jupiter.api.Assertions.assertEquals
+import static org.junit.jupiter.api.Assertions.assertTrue
+
+/**
+ * https://github.com/lazystone/spotbugs-bugs/blob/master/build.gradle.kts
+ * https://github.com/spotbugs/spotbugs-gradle-plugin/issues/196
+ */
+class Issue196 extends Specification {
+    File rootDir
+    File buildFile
+    String version = System.getProperty('snom.test.functional.gradle', GradleVersion.current().version)
+
+    @BeforeEach
+    def setup() {
+        rootDir = Files.createTempDir()
+        buildFile = new File(rootDir, 'build.gradle.kts')
+        buildFile << """
+plugins {
+  id("com.github.spotbugs")
+  java
+}
+repositories {
+  mavenCentral()
+}
+        """
+        File sourceDir = rootDir.toPath().resolve("src").resolve("main").resolve("java").toFile()
+        sourceDir.mkdirs()
+        File sourceFile = new File(sourceDir, "Foo.java")
+        sourceFile << """
+public class Foo {
+    public static void main(String... args) {
+        System.out.println("Hello, SpotBugs!");
+    }
+}
+"""
+    }
+
+    def "can run analysis when check task is triggered"() {
+        when:
+        BuildResult result =
+                GradleRunner.create()
+                .withProjectDir(rootDir)
+                .withArguments("check", "--debug")
+                .withPluginClasspath()
+                .forwardOutput()
+                .withGradleVersion(version)
+                .build()
+
+        then:
+        result.task(":spotbugsMain").outcome == TaskOutcome.SUCCESS
+    }
+}
