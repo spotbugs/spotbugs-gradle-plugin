67052058c3cd85edcef911930da6f99d4624d1c7
Kengo TODA <skypencil+github@gmail.com>
2021-09-08 16:21:48 +0800

    build: try to fix build script for Gradle 5.x

--- buildSrc/src/main/kotlin/com/github/spotbugs/com.github.spotbugs.gradle-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/com/github/spotbugs/com.github.spotbugs.gradle-plugin.gradle.kts
@@ -5,15 +5,9 @@ plugins {
     id("net.ltgt.errorprone")
 }
 
-if (GradleVersion.current() < GradleVersion.version("6.0")) {
-    tasks.withType<JavaCompile>().configureEach {
-        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-        targetCompatibility = JavaVersion.VERSION_1_8.toString()
-    }
-}
-
 tasks.withType<JavaCompile>().configureEach {
-    options.release.set(8)
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
 
     // disable warnings in generated code by immutables
     // https://github.com/google/error-prone/issues/329
