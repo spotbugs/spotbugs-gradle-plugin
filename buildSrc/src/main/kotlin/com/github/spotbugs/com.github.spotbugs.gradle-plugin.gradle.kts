203758c1137d843279549781f1c0f40fd7dd2d8a
Kengo TODA <skypencil@gmail.com>
2021-09-08 17:51:50 +0800

    build: migrate more build scripts to Kotlin

--- buildSrc/src/main/kotlin/com/github/spotbugs/com.github.spotbugs.gradle-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/com/github/spotbugs/com.github.spotbugs.gradle-plugin.gradle.kts
@@ -5,10 +5,17 @@ plugins {
     id("net.ltgt.errorprone")
 }
 
-// disable warnings in generated code by immutables
-// https://github.com/google/error-prone/issues/329
 tasks.withType<JavaCompile>().configureEach {
+    // disable warnings in generated code by immutables
+    // https://github.com/google/error-prone/issues/329
     options.errorprone.disableWarningsInGeneratedCode.set(true)
+}
+
+tasks.withType<Groovydoc>().configureEach {
+    docTitle = "SpotBugs Gradle Plugin"
+    link("https://docs.gradle.org/current/javadoc/", "org.gradle.api.")
+    link("https://docs.oracle.com/en/java/javase/11/docs/api/", "java.")
+    link("https://docs.groovy-lang.org/latest/html/gapi/", "groovy.", "org.codehaus.groovy.")
 }
 
 spotless {
