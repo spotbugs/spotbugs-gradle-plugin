0069e1ce83be5f5520b58eb2bbd77bcde529fc22
Kengo TODA <skypencil@gmail.com>
2021-10-28 17:24:32 +0800

    test: simplify test cases

--- src/functionalTest/groovy/com/github/spotbugs/snom/KotlinBuildScriptFunctionalTest.groovy
+++ src/functionalTest/groovy/com/github/spotbugs/snom/KotlinBuildScriptFunctionalTest.groovy
@@ -111,9 +111,7 @@ dependencies {
 
         then:
         result.task(":spotbugsMain").outcome == SUCCESS
-        if (GradleVersion.version(version) >= GradleVersion.version("6.0")) {
-            result.output.contains("Applying com.h3xstream.findsecbugs.PredictableRandomDetector to Foo")
-        }
+        result.output.contains("Applying com.h3xstream.findsecbugs.PredictableRandomDetector to Foo")
         !result.output.contains("Trying to add already registered factory")
     }
 
