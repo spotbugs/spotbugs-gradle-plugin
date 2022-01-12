91b7259c7bf9efc742eaf90735862333411598a9
Kengo TODA <skypencil+github@gmail.com>
2022-01-12 09:16:48 +0800

    test: reproduce the reported problem as a functional test
    refs #662

--- src/functionalTest/groovy/com/github/spotbugs/snom/CacheabilityFunctionalTest.groovy
+++ src/functionalTest/groovy/com/github/spotbugs/snom/CacheabilityFunctionalTest.groovy
@@ -16,13 +16,34 @@ package com.github.spotbugs.snom
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.util.GradleVersion
-import spock.lang.Requires
 import spock.lang.Specification
 
 import java.nio.file.Files
 
 class CacheabilityFunctionalTest extends Specification {
+    /**
+     * @see <a href="https://github.com/spotbugs/spotbugs-gradle-plugin/issues/662">GitHub Issues</a>
+     */
+    def 'spotbugsMain task runs with configuration cache'() {
+        given:
+        def buildDir = Files.createTempDirectory(null).toFile()
+        def version = System.getProperty('snom.test.functional.gradle', GradleVersion.current().version)
 
+        initializeBuildFile(buildDir)
+
+        when:
+        BuildResult result =
+                GradleRunner.create()
+                        .withProjectDir(buildDir)
+                        .withArguments(':spotbugsMain', '--configuration-cache')
+                        .withPluginClasspath()
+                        .forwardOutput()
+                        .withGradleVersion(version)
+                        .build()
+
+        then:
+        !result.output.contains("Configuration cache problems found in this build")
+    }
 
     /**
      * Verifies the cacheability of {@link SpotBugsTask} by invoking the same code
@@ -42,8 +63,8 @@ class CacheabilityFunctionalTest extends Specification {
 
         def version = System.getProperty('snom.test.functional.gradle', GradleVersion.current().version)
 
-        initializeBuildFile(buildDir1, !version.startsWith('5'))
-        initializeBuildFile(buildDir2, !version.startsWith('5'))
+        initializeBuildFile(buildDir1)
+        initializeBuildFile(buildDir2)
 
         when:
         BuildResult result1 =
@@ -79,7 +100,7 @@ class CacheabilityFunctionalTest extends Specification {
         return result.output.find('Build cache key for task \':spotbugsMain\' is .*')
     }
 
-    private static void initializeBuildFile(File buildDir, boolean runScan) {
+    private static void initializeBuildFile(File buildDir) {
         File buildFile = new File(buildDir, 'build.gradle')
         File settingsFile = new File(buildDir, 'settings.gradle')
         File propertiesFile = new File(buildDir, 'gradle.properties')
@@ -102,19 +123,17 @@ class CacheabilityFunctionalTest extends Specification {
             |}
             |'''.stripMargin()
 
-        if (runScan) {
-            settingsFile << '''
-                |plugins {
-                |    id "com.gradle.enterprise" version "3.6.4"
-                |}
-                |gradleEnterprise {
-                |    buildScan {
-                |        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-                |        termsOfServiceAgree = "yes"
-                |    }
-                |}
-                '''.stripMargin()
-        }
+        settingsFile << '''
+            |plugins {
+            |    id "com.gradle.enterprise" version "3.6.4"
+            |}
+            |gradleEnterprise {
+            |    buildScan {
+            |        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            |        termsOfServiceAgree = "yes"
+            |    }
+            |}
+            '''.stripMargin()
         File sourceDir = buildDir.toPath().resolve('src').resolve('main').resolve('java').toFile()
         sourceDir.mkdirs()
         File sourceFile = new File(sourceDir, 'Foo.java')
