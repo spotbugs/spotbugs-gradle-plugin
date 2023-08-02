12d40358ff6ccabe832e9177370635131d003e6e
Kengo TODA <skypencil@gmail.com>
2023-08-03 04:56:51 +0800

    build: update build settings
    Signed-off-by: Kengo TODA <skypencil@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 group = "com.github.spotbugs.snom"
 
