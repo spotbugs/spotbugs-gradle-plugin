5db6b1ed244e270f7a0f4d6080fcc48d0b626cea
Kengo TODA <skypencil@gmail.com>
2023-08-10 18:09:26 +0800

    build: remove needless spotless rules
    Signed-off-by: Kengo TODA <skypencil@gmail.com>

--- buildSrc/src/main/kotlin/com/github/spotbugs/com.github.spotbugs.gradle-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/com/github/spotbugs/com.github.spotbugs.gradle-plugin.gradle.kts
@@ -5,22 +5,6 @@ plugins {
 // TODO introduce KDoc
 
 spotless {
-    java {
-        licenseHeaderFile("gradle/HEADER.txt")
-        removeUnusedImports()
-        googleJavaFormat()
-    }
-    groovy {
-        licenseHeaderFile("gradle/HEADER.txt")
-        target("**/*.groovy")
-        greclipse()
-        indentWithSpaces()
-    }
-    groovyGradle {
-        target("**/*.gradle")
-        greclipse()
-        indentWithSpaces()
-    }
     kotlin {
         ktlint()
     }
