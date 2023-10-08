34eb86699720e9757013c907eb6a42dcea4a6242
Goooler <wangzongler@gmail.com>
2023-10-08 10:26:09 +0800

    Always publish build scans on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,12 @@ plugins {
 
 rootProject.name = "spotbugs-gradle-plugin"
 
+val isCiBuild = providers.environmentVariable("CI").isPresent
+
 gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
+        publishAlwaysIf(isCiBuild)
     }
 }
