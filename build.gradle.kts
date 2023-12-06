bc8a8d3aa76a7b61e042b5849fcc62275e0988bd
Goooler <wangzongler@gmail.com>
2023-12-06 11:01:34 +0800

    Apply org.jetbrains.kotlinx.binary-compatibility-validator
    For better API compatibilities check.
    
    https://github.com/Kotlin/binary-compatibility-validator

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     id("com.github.spotbugs.test")
     id("org.sonarqube")
     id("io.gitlab.arturbosch.detekt") version "1.23.4"
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.2"
 }
 
 java {
