cb838fc19e64a8cad44503091cb1a109f06d61e1
Goooler <wangzongler@gmail.com>
2023-12-11 21:09:33 +0800

    Reuse testKitDir
    Use this to share test dependencies cache for speeding up clean build.
    
    https://docs.gradle.org/8.5/javadoc/org/gradle/testkit/runner/GradleRunner.html#withTestKitDir-java.io.File-

--- src/functionalTest/groovy/com/github/spotbugs/snom/BaseFunctionalTest.groovy
+++ src/functionalTest/groovy/com/github/spotbugs/snom/BaseFunctionalTest.groovy
@@ -30,6 +30,7 @@ abstract class BaseFunctionalTest extends Specification {
                 .withGradleVersion(gradleVersion)
                 .withProjectDir(rootDir)
                 .withArguments('--info', '--stacktrace', '--warning-mode=fail')
+                .withTestKitDir(testKitDir)
                 .forwardOutput()
                 .withPluginClasspath()
     }
@@ -44,5 +45,13 @@ abstract class BaseFunctionalTest extends Specification {
         DefaultGradleRunner withArguments(String... arguments) {
             return withArguments(Arrays.asList(arguments))
         }
+    }
+
+    private static File getTestKitDir() {
+        def gradleUserHome = System.getenv("GRADLE_USER_HOME")
+        if (!gradleUserHome) {
+            gradleUserHome = new File(System.getProperty("user.home"), ".gradle").absolutePath
+        }
+        return new File(gradleUserHome, "testkit")
     }
 }
